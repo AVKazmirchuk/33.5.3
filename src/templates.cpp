2020547b0eb1666fce01746d839364265ee74ce8
AVKazmirchuk <85755971+AVKazmirchuk@users.noreply.github.com>
2021-11-14 20:09:24 +0300

    Add files via upload

---
+++ src/templates.cpp
@@ -0,0 +1,12 @@
+#include "../include/header.h"
+#include "Source.cpp"
+
+template class Registry::Map<int, int>;
+template class Registry::Map<int, double>;
+template class Registry::Map<int, std::string>;
+template class Registry::Map<double, int>;
+template class Registry::Map<double, double>;
+template class Registry::Map<double, std::string>;
+template class Registry::Map<std::string, int>;
+template class Registry::Map<std::string, double>;
+template class Registry::Map<std::string, std::string>;
